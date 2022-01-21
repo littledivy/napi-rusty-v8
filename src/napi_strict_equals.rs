574c4748e0c687fc99ef38c805af4d095cb9c781
DjDeveloper <43033058+DjDeveloperr@users.noreply.github.com>
2022-01-21 15:49:15 +0000

    impl napi_strict_equals

--- src/napi_strict_equals.rs
+++ src/napi_strict_equals.rs
@@ -1,4 +1,17 @@
+use crate::env::Env;
+use crate::ffi::*;
+use deno_core::v8;
+
 #[no_mangle]
-pub unsafe extern "C" fn napi_strict_equals() {
-  todo!()
+pub unsafe extern "C" fn napi_strict_equals(
+  env: napi_env,
+  lhs: napi_value,
+  rhs: napi_value,
+  result: *mut bool,
+) -> napi_status {
+  let mut env = &mut (env as *mut Env);
+  let lhs: v8::Local<v8::Value> = std::mem::transmute(lhs);
+  let rhs: v8::Local<v8::Value> = std::mem::transmute(rhs);
+  *result = lhs.strict_equals(rhs);
+  napi_ok
 }
