c16c1f1b24fdf5bcc149a57e40c078f4965c6a4e
Divy Srivastava <dj.srivastava23@gmail.com>
2022-01-22 19:15:33 +0530

    add #[napi_sym] macro

--- src/napi_create_string_utf8.rs
+++ src/napi_create_string_utf8.rs
@@ -2,13 +2,13 @@ use crate::env::Env;
 use crate::ffi::*;
 use deno_core::v8;
 
-#[no_mangle]
-pub unsafe extern "C" fn napi_create_string_utf8(
+#[napi_sym::napi_sym]
+fn napi_create_string_utf8(
   env: napi_env,
   string: *const u8,
   length: usize,
   result: *mut napi_value,
-) -> napi_status {
+) -> Result<(), ()> {
   let mut env = &mut *(env as *mut Env);
 
   let string = std::slice::from_raw_parts(string, length);
@@ -18,5 +18,5 @@ pub unsafe extern "C" fn napi_create_string_utf8(
   let value: v8::Local<v8::Value> = v8str.into();
   *result = std::mem::transmute(value);
 
-  napi_ok
+  Ok(())
 }
