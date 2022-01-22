2bae685f36000e2a4a6711a3814c52eef75764a3
DjDeveloper <43033058+DjDeveloperr@users.noreply.github.com>
2022-01-22 08:28:51 +0000

    impl napi_create_range_error

--- src/napi_create_range_error.rs
+++ src/napi_create_range_error.rs
@@ -1,4 +1,23 @@
+use crate::env::Env;
+use crate::ffi::*;
+use deno_core::v8;
+
 #[no_mangle]
-pub unsafe extern "C" fn napi_create_range_error() {
-  todo!()
+pub unsafe extern "C" fn napi_create_range_error(
+  env: napi_env,
+  code: napi_value,
+  msg: napi_value,
+  result: *mut napi_value,
+) -> napi_status {
+  let mut env = &mut *(env as *mut Env);
+
+  let code: v8::Local<v8::Value> = std::mem::transmute(code);
+  let msg: v8::Local<v8::Value> = std::mem::transmute(msg);
+
+  let msg = msg.to_string(env.scope).unwrap();
+
+  let error = v8::Exception::range_error(env.scope, msg);
+  *result = std::mem::transmute(error);
+
+  napi_ok
 }
