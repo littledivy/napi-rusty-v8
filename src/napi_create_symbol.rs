8e81d4bf1005559d5f6bd7962033b95396aa83fa
DjDeveloper <43033058+DjDeveloperr@users.noreply.github.com>
2022-01-22 13:00:15 +0000

    impl napi_create_symbol

--- src/napi_create_symbol.rs
+++ src/napi_create_symbol.rs
@@ -1,13 +1,22 @@
 use crate::env::Env;
 use crate::ffi::*;
+use deno_core::v8;
 
-// TODO: properly implement
 #[no_mangle]
 pub unsafe extern "C" fn napi_create_symbol(
   env: napi_env,
   description: napi_value,
   result: *mut napi_value,
 ) -> napi_status {
-  let mut _env = &mut *(env as *mut Env);
+  let mut env = &mut *(env as *mut Env);
+  let description = match result.is_null() {
+    true => None,
+    false => Some(
+      std::mem::transmute::<napi_value, v8::Local<v8::Value>>(description)
+        .to_string(env.scope)
+        .unwrap(),
+    ),
+  };
+  let sym = v8::Symbol::new(env.scope, description);
   napi_ok
 }
