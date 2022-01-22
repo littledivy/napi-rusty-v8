9cff3e0c9e249a7e10e87a04f1e2e0b991904140
DjDeveloper <43033058+DjDeveloperr@users.noreply.github.com>
2022-01-22 04:44:42 +0000

    impl napi_set_element

--- src/napi_set_element.rs
+++ src/napi_set_element.rs
@@ -1,4 +1,18 @@
+use crate::env::Env;
+use crate::ffi::*;
+use deno_core::v8;
+
 #[no_mangle]
-pub unsafe extern "C" fn napi_set_element() {
-  todo!()
+pub unsafe extern "C" fn napi_set_element(
+  env: napi_env,
+  object: napi_value,
+  index: u32,
+  value: napi_value,
+) -> napi_status {
+  let mut env = &mut *(env as *mut Env);
+  let object: v8::Local<v8::Value> = std::mem::transmute(object);
+  let array = v8::Local::<v8::Array>::try_from(object).unwrap();
+  let value: v8::Local<v8::Value> = std::mem::transmute(value);
+  array.set_index(env.scope, index, value).unwrap();
+  napi_ok
 }
