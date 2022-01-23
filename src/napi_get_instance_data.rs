df02d2e8f41624fa4ae097814ce679bf71ca64d5
DjDeveloper <43033058+DjDeveloperr@users.noreply.github.com>
2022-01-23 08:26:23 +0000

    impl instance data

--- src/napi_get_instance_data.rs
+++ src/napi_get_instance_data.rs
@@ -1,4 +1,13 @@
+use crate::env::Env;
+use crate::ffi::*;
+
 #[no_mangle]
-pub unsafe extern "C" fn napi_get_instance_data() {
-  todo!()
+pub unsafe extern "C" fn napi_get_instance_data(
+  env: napi_env,
+  result: *mut *mut c_void,
+) -> napi_status {
+  let mut env = &mut *(env as *mut Env);
+  let shared = env.shared();
+  *result = shared.instance_data;
+  env.ok()
 }
