b12a248ea520cb505e06aaa3db769b9c3e80bc46
DjDeveloper <43033058+DjDeveloperr@users.noreply.github.com>
2022-01-23 14:30:07 +0000

    impl napi_has_property

--- src/main.rs
+++ src/main.rs
@@ -100,6 +100,7 @@ pub mod napi_get_value_string_utf8;
 pub mod napi_get_value_string_utf16;
 pub mod napi_get_value_uint32;
 pub mod napi_get_version;
+pub mod napi_has_property;
 pub mod napi_instanceof;
 pub mod napi_is_array;
 pub mod napi_is_arraybuffer;
---
+++ src/napi_has_property.rs
@@ -0,0 +1,19 @@
+use crate::env::Env;
+use crate::ffi::*;
+use deno_core::v8;
+
+#[no_mangle]
+pub unsafe extern "C" fn napi_has_property(
+  env: napi_env,
+  value: napi_value,
+  key: napi_value,
+  result: *mut bool,
+) -> napi_status {
+  let mut env = &mut *(env as *mut Env);
+  let value: v8::Local<v8::Value> = std::mem::transmute(value);
+  let obj = value.to_object(env.scope).unwrap();
+  *result = obj
+    .has(env.scope, std::mem::transmute(key))
+    .unwrap_or(false);
+  napi_ok
+}
