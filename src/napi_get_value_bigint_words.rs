3287d04f73da9ecaae5cf0c0861da8f254902e8f
DjDeveloper <43033058+DjDeveloperr@users.noreply.github.com>
2022-01-23 13:35:34 +0000

    impl bigint value getters

---
+++ src/napi_get_value_bigint_words.rs
@@ -0,0 +1,28 @@
+use crate::env::Env;
+use crate::ffi::*;
+use deno_core::v8;
+
+#[no_mangle]
+pub unsafe extern "C" fn napi_get_value_bigint_words(
+  env: napi_env,
+  value: napi_value,
+  sign_bit: *mut i32,
+  size: *mut usize,
+  out_words: *mut u64,
+) -> napi_status {
+  let mut env = &mut *(env as *mut Env);
+  
+  let value: v8::Local<v8::Value> = std::mem::transmute(value);
+  let bigint = value.to_big_int(env.scope).unwrap();
+
+  let out_words = std::slice::from_raw_parts_mut(out_words, *size);
+  let mut words = Vec::with_capacity(bigint.word_count());
+  let (sign, _) = bigint.to_words_array(words.as_mut_slice());
+  *sign_bit = sign as i32;
+
+  for (i, word) in out_words.iter_mut().enumerate() {
+    *word = words[i];
+  }
+
+  napi_ok
+}
